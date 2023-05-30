2a8c4d7c013f26809125b73ec43f320f6d0b37af
Timb42 <133685540+Timb42@users.noreply.github.com>
2023-05-30 15:03:57 -0300

    Co-authored-by: como2872 <como2872@users.noreply.github.com>

---
+++ Assignment/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputSets = document.querySelector('.form__input--sets'); //  distance
+const inputReps = document.querySelector('.form__input--reps'); //  duration
+const inputRestTime = document.querySelector('.form__input--rest_time'); // cadence
